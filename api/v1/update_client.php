fe8836be3dfe776d5dbf3075fe15f24def9497a9
BrandonGS <brandongs180@gmail.com>
2021-05-16 22:32:27 -0500

    Add client endpoints

---
+++ api/v1/update_client.php
@@ -0,0 +1,63 @@
+<?php
+
+include "../config/database.php";
+include "../helpers/token.php";
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+  header('Access-Control-Allow-Origin: *');
+  header('Access-Control-Allow-Methods: POST, GET, DELETE, PUT, PATCH, OPTIONS');
+  header('Access-Control-Allow-Headers: Authorization, Content-Type');
+  header('Access-Control-Max-Age: 1728000');
+  die();
+}
+
+header('Access-Control-Allow-Origin: *');
+
+
+$headers = apache_request_headers();
+
+if (!$headers["Authorization"]) {
+  $response = [
+    'message' => "No authorization header"
+  ];
+  echo json_encode($response);
+  header("HTTP/1.1 401 No authorization header");
+  return;
+}
+
+$token_auth = $headers["Authorization"];
+
+if (!$token_auth) {
+  $response = [
+    'message' => "Debe tener autorizacion, contacte con el administrador."
+  ];
+  echo json_encode($response);
+  header("HTTP/1.1 401 Not authorization");
+  return;
+}
+
+try {
+  $token_auth = $headers["Authorization"];
+  $token_decode = decodeToken($token_auth);
+
+  $id = $_POST["id"];
+  $name = $_POST["name"];
+
+  $query = "UPDATE client SET client_name='$name' WHERE client_id='$id'";
+  api_post($query);
+
+  $response = [
+    'message' => "Cliente actualizado",
+  ];
+
+  echo json_encode($response);
+  header("HTTP/1.1 200 OK");
+} catch(Exception $e) {
+  $response = [
+    'message' => $e
+  ];
+  echo json_encode($response);
+  header("HTTP/1.1 401 '$e'");
+}
+
+?>
