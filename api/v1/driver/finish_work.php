3b7d601cbb14c474c650988430238e4851c95e7e
BrandonGS <brandongs180@gmail.com>
2021-05-21 23:09:56 -0500

    Add finish work

---
+++ api/v1/driver/finish_work.php
@@ -0,0 +1,62 @@
+<?php
+
+include __DIR__."/../../config/database.php";
+include __DIR__."/../../helpers/token.php";
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+  header('Access-Control-Allow-Origin: *');
+  header('Access-Control-Allow-Methods: POST, GET, DELETE, PUT, PATCH, OPTIONS');
+  header('Access-Control-Allow-Headers: authorization, Content-Type');
+  header('Access-Control-Max-Age: 1728000');
+  die();
+}
+
+header('Access-Control-Allow-Origin: *');
+
+
+$headers = apache_request_headers();
+
+if (!$headers["authorization"]) {
+  $response = [
+    'message' => "No authorization header"
+  ];
+  echo json_encode($response);
+  header("HTTP/1.1 401 No authorization header");
+  return;
+}
+
+$token_auth = $headers["authorization"];
+
+if (!$token_auth) {
+  $response = [
+    'message' => "Debe tener autorizacion, contacte con el administrador."
+  ];
+  echo json_encode($response);
+  header("HTTP/1.1 401 Not authorization");
+  return;
+}
+
+try {
+  $token_auth = $headers["authorization"];
+  $token_decode = decodeToken($token_auth);
+
+  $id = $_POST["id"];
+
+  $query = "UPDATE driver_work SET driver_work_finished=true WHERE driver_work_id='$id'";
+  api_post($query);
+
+  $response = [
+    'message' => "Trabajo terminado",
+  ];
+
+  echo json_encode($response);
+  header("HTTP/1.1 200 OK");
+} catch(Exception $e) {
+  $response = [
+    'message' => $e
+  ];
+  echo json_encode($response);
+  header("HTTP/1.1 401 '$e'");
+}
+
+?>
